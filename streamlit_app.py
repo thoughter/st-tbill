2d0541f37a2594045cecee3508e87a298a07687d
Jack Yang <westc@DESKTOP-HA70RVO.localdomain>
2024-04-24 21:54:53 -0400

    .

--- streamlit_app.py
+++ streamlit_app.py
@@ -14,6 +14,17 @@ if(price > 0 and price < 100 and d2 > d1):
     ndays = (d2 - d1).days
     yld = (100.0 - price )/ price
     ret = yld * 365 / ndays
-    st.write(f'The yield is {round(ret*100,2)}%')
+    st.write(f'The return is {round(ret*100,2)}%, {ndays} days, yield = {yld}')
 
 
+st.title('Option Return')
+st_price = st.number_input("Strike Price")
+opt_price = st.number_input("Option Price")
+exp_date = st.date_input("Expire Date", datetime.date.today())
+
+if(st_price!=0 and opt_price!=0 and exp_date>datetime.date.today()):
+
+    ndays = (exp_date-datetime.date.today()).days
+    yld = opt_price / st_price
+    ret = yld * 365 / ndays
+    st.write(f'The return is {round(ret*100,2)}%, {ndays} days, yield = {yld}')
